e8bcae9d4b6609abfd8a49925b391591d3f50915
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-06-18 10:14:57 +0900

    fix config

--- tests/config/config.ts
+++ tests/config/config.ts
@@ -6,9 +6,20 @@ interface Config {
 }
 
 const options: Options = {
-  vus: 10,
-  duration: "5s",
+  stages: [
+    // 合計で3秒間、秒間66リクエストの負荷試験
+    { duration: '1s', target: 66 }, //1秒間に66のユーザーがリクエストを送信し続け
+    { duration: '1s', target: 66 }, //1秒間に66のユーザーがリクエストを送信し続け
+    { duration: '1s', target: 66 }, //1秒間に66のユーザーがリクエストを送信し続け
+  ],
+  // vus: 10,
+  // duration: "5s",
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // httpエラーは1%未満であるべき
+    http_req_duration: ['p(95)<200'], // リクエストの95%は200ms以下であること
+  },
 };
+
 
 export const config: Config = {
   urlbase: "https://example.com",
